55fa2ce2995ba8eeb6073306c3dce4274fe3487e
vilenarios <vilenarios@gmail.com>
2022-07-08 19:57:48 -0400

    Fixes evolution with new warp-contract code

--- src/tools/evolve-contract.ts
+++ src/tools/evolve-contract.ts
@@ -3,16 +3,17 @@ import { LoggerFactory, WarpNodeFactory } from "warp-contracts";
 import * as fs from "fs";
 import path from "path";
 import { JWKInterface } from "arweave/node/lib/wallet";
-import { deployedTestContracts } from "../deployed-contracts";
-import { testKeyfile } from "../constants";
+// import { deployedContracts } from "../deployed-contracts";
+import { keyfile } from "../constants";
 
 (async () => {
-  // This is the testnet ArNS Registry Smartweave Contract TX ID
-  const arnsRegistryContractTxId = deployedTestContracts.contractTxId;
+  // This is the mainnet ArNS Registry Smartweave Contract TX ID
+  const arnsRegistryContractTxId = "Ydv5pDoM6NBY1hySaWiRbe_2L3aeDDoLB7QNIu4BZpw";
 
   // Initialize Arweave
   const arweave = Arweave.init({
-    host: "testnet.redstone.tools",
+    host: "arweave.net",
+    timeout: 600000,
     port: 443,
     protocol: "https",
   });
@@ -25,23 +26,27 @@ import { testKeyfile } from "../constants";
 
   // Get the key file used
   const wallet: JWKInterface = JSON.parse(
-    await fs.readFileSync(testKeyfile).toString()
+    await fs.readFileSync(keyfile).toString()
   );
 
   // Read the ArNS Registry Contract
   const pst = smartweave.pst(arnsRegistryContractTxId);
   pst.connect(wallet);
 
-  // ~~ Read test contract source and initial state files ~~
+  // ~~ Read contract source and initial state files ~~
   const newSource = fs.readFileSync(
     path.join(__dirname, "../../dist/contract.js"),
     "utf8"
   );
+  console.log (newSource)
   const newSrcTxId = await pst.save({ src: newSource });
   if (newSrcTxId === null) {
     return 0;
   }
-  await pst.evolve(newSrcTxId);
+  console.log (newSrcTxId)
+  const evolvedTxId = await pst.evolve(newSrcTxId);
 
-  console.log("Finished evolving the Test ArNS Smartweave Contract %s.", newSrcTxId);
+  console.log("Finished evolving the ArNS Smartweave Contract %s.", newSrcTxId);
+  console.log(`New Contract Tx Id ${evolvedTxId}`);
+
 })();
