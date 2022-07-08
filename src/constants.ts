43caa710b917fd2772d94ae8443cef5edcd7c147
vilenarios <vilenarios@gmail.com>
2022-07-08 19:58:06 -0400

    Changes max name from 20 to 32

--- src/constants.ts
+++ src/constants.ts
@@ -2,12 +2,12 @@ export const url = {
   redstoneGateway: "",
 };
 
-export const MAX_NAME_LENGTH = 20;
+export const MAX_NAME_LENGTH = 32;
 export const TX_ID_LENGTH = 43;
 
 // The mainnet wallet that will be used in the creation of contracts, ants and record purchases.
 export const keyfile =
-  "";
+  "F:\\Keys\\ARNS3_arweave-keyfile-QGWqtJdLLgm2ehFWiiPzMaoFLD50CnGuzZIPEdoDRGQ.json";
 
   // The Redstone Testnet wallet that will be used in the creation of contracts, ants and record purchases.
 export const testKeyfile =
