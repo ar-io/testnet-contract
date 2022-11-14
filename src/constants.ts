23983825d2a48f5a2f43d546ad34ac262cf32922
Philip Mataras <vilenarios@gmail.com>
2022-11-14 08:26:24 -0500

    updated warp

--- src/constants.ts
+++ src/constants.ts
@@ -15,4 +15,4 @@ export const keyfile =
   "F:\\Keys\\ARNS3_arweave-keyfile-QGWqtJdLLgm2ehFWiiPzMaoFLD50CnGuzZIPEdoDRGQ.json";
 
 // The Redstone Testnet wallet that will be used in the creation of contracts, ants and record purchases.
-export const testKeyfile = "";
+export const testKeyfile = "C:\\STUFF\\GNSR3_arweave-keyfile-QGWqtJdLLgm2ehFWiiPzMaoFLD50CnGuzZIPEdoDRGQ.json";
