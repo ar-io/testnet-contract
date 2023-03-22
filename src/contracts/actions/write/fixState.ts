6b0ea82b03e5a8a99a9309b1d11bb8219ee589e4
dtfiedler <dylan@ar.io>
2023-03-22 08:16:49 -0600

    chore: some fix state cleanup

--- src/contracts/actions/write/fixState.ts
+++ src/contracts/actions/write/fixState.ts
@@ -1,4 +1,4 @@
-import { SECONDS_IN_A_YEAR } from '@/constants';
+import { DEFAULT_NON_CONTRACT_OWNER_MESSAGE, SECONDS_IN_A_YEAR } from '@/constants';
 
 import { ContractResult, IOState, PstAction } from '../../types/types';
 
@@ -8,17 +8,16 @@ declare const SmartWeave: any;
 // Temporary method to fix a broken contract state
 export const fixState = async (
   state: IOState,
-  { caller, input: {} }: PstAction, // eslint-disable-line
+  { caller }: PstAction,
 ): Promise<ContractResult> => {
   const owner = state.owner;
 
   if (caller !== owner) {
-    throw new ContractError('Caller cannot evolve the contract');
+    throw new ContractError(DEFAULT_NON_CONTRACT_OWNER_MESSAGE);
   }
 
   for (const key of Object.keys(state.records)) {
     if (state.records[key].contractTxId === undefined) {
-      // set the end lease period for this based on number of years
       const endTimestamp = +SmartWeave.block.timestamp;
       +SECONDS_IN_A_YEAR * 1; // default tier
       state.records[key] = {
