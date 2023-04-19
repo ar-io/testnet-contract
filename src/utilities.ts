05b5759eb5250a0cbe109cf6f8016cfdb8983192
Philip Mataras <vilenarios@gmail.com>
2023-04-19 09:20:54 -0400

    Adds method to check if string is valid arweave base64url

--- src/utilities.ts
+++ src/utilities.ts
@@ -41,3 +41,9 @@ export function isValidFQDN(fqdn: string) {
   const fqdnRegex = /^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)+[A-Za-z]{2,6}$/;
   return fqdnRegex.test(fqdn);
 }
+
+// check if it is a valid arweave base64url for a wallet public address, transaction id or smartweave contract
+export function isValidArweaveBase64URL(base64URL: string) {
+  const base64URLRegex = new RegExp('^[a-zA-Z0-9_-]{43}$');
+  return base64URLRegex.test(base64URL);
+}
