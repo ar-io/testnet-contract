b64b9f150c72709d8b4c664289d9e7fa812a1d0c
Dylan Fiedler <dtfiedler@users.noreply.github.com>
2023-05-12 09:03:16 -0700

    feat(PE-3547): add foundation and functions (#24)
    * Adds types to support foundation actions
    
    * wip
    
    * wip
    
    * wip
    
    * Adds tests for adding and removing foundation wallets
    
    * fix(pst): update balance function to be PST compliant, and modify types to be more explicit' (#22) (#23)
    
    * fix(pst): update functions to be pst compliant
    
    Moving 'balance' to 'getBalance' no longer adheres to PST requirements. Updating functions and breaking apart types, so these are explicit. We should continue this pattern going forward.
    
    * chore: formatting
    
    * chore(tests): update existing tests and add checks for balances
    
    * chore: linting
    
    ---------
    
    Co-authored-by: Dylan Fiedler <dtfiedler@users.noreply.github.com>
    Co-authored-by: dtfiedler <dylan@ar.io>
    
    * Isolates foundation types
    
    * adds more tests
    
    * wip
    
    * wip
    
    * Wip
    
    * changed to value inputs
    
    * adds tests for setting active tiers
    
    * Fixes typo
    
    * initiate/sign foundation actions finished with tests
    
    * Fixed extra variables
    
    * Using const instead of let
    
    * Adds verification of undernames in setNameFees
    
    * Adds types to support foundation actions
    
    * wip
    
    * wip
    
    * wip
    
    * Adds tests for adding and removing foundation wallets
    
    * Isolates foundation types
    
    * adds more tests
    
    * wip
    
    * wip
    
    * Wip
    
    * changed to value inputs
    
    * adds tests for setting active tiers
    
    * Fixes typo
    
    * initiate/sign foundation actions finished with tests
    
    * Fixed extra variables
    
    * Using const instead of let
    
    * Adds verification of undernames in setNameFees
    
    * adds new types and case statements
    
    * Adds more switches
    
    * Removes unused types
    
    * wip for foundation evolve with delay
    
    * changes input from id to value
    
    * includes more tests for evolve
    
    * cleans up variable name
    
    * Adds another test
    
    * ran prettier
    
    * merged initiate and sign foundation action
    
    * lint and formatting rule fixes
    
    * Adds null coalense for foundation actions
    
    * changes to startHeight for foundationAction
    
    * removes unneeded default constants
    
    * Removes uneeded duplicate function call
    
    * Updates ID from number to string containing the smartweave tx id of the interaction
    
    * formatting/linting
    
    * chore: add more tests, restructure a bit
    
    ---------
    
    Co-authored-by: Philip Mataras <vilenarios@gmail.com>
    Co-authored-by: dtfiedler <dylan@ar.io>

--- src/actions/write/evolve.ts
+++ src/actions/write/evolve.ts
@@ -1,22 +1,46 @@
-import { DEFAULT_NON_CONTRACT_OWNER_MESSAGE } from '../../constants';
-import { ContractResult, IOState, PstAction } from '../../types';
+import {
+  FOUNDATION_ACTION_PASSED_STATUS,
+  FOUNDATION_DELAYED_EVOLVE_COMPLETED_STATUS,
+} from '../../constants';
+import {
+  ContractResult,
+  DelayedEvolveInput,
+  FoundationAction,
+  IOState,
+  PstAction,
+} from '../../types';
 
 declare const ContractError;
+declare const SmartWeave: any;
 
 // Updates this contract to new source code
 export const evolve = async (
   state: IOState,
-  { caller, input: { value } }: PstAction,
+  { input }: PstAction,
 ): Promise<ContractResult> => {
-  const owner = state.owner;
+  const foundationActions = state.foundation?.actions ?? [];
+  const actionId = input.value as string;
+  const action: FoundationAction = foundationActions.find(
+    (action) => action.id === actionId,
+  );
+  const actionIndex = foundationActions.indexOf(action);
 
-  if (caller !== owner) {
-    throw new ContractError(DEFAULT_NON_CONTRACT_OWNER_MESSAGE);
+  // There are no caller checks, as anyone can invoke the evolutiona as long as conditions are met
+  if (
+    action &&
+    action.type === 'delayedEvolve' &&
+    action.status === FOUNDATION_ACTION_PASSED_STATUS &&
+    (action.value as DelayedEvolveInput).evolveHeight <=
+      +SmartWeave.block.height
+  ) {
+    state.foundation.actions[actionIndex].status =
+      FOUNDATION_DELAYED_EVOLVE_COMPLETED_STATUS;
+    state.evolve = (
+      foundationActions[actionIndex].value as DelayedEvolveInput
+    ).contractSrcTxId;
+  } else {
+    throw new ContractError('Invalid contract evolution operation.');
   }
-
-  // TODO: regex on the string
-
-  state.evolve = value.toString();
 
   return { state };
 };
