b8a5bf32a78c77d8ce2ae47eba608ee7eb8a82e8
dtfiedler <dylan@ar.io>
2023-05-12 21:26:24 -0600

    chore: remove mintTokends and add revert eslint upgrade

--- src/actions/read/balance.ts
+++ src/actions/read/balance.ts
@@ -4,12 +4,9 @@ declare const ContractError;
 
 export const balance = async (
   state: IOState,
-  { input }: PstAction,
+  { input: { target } }: PstAction,
 ): Promise<ContractResult> => {
   const balances = state.balances;
-
-  // TODO: object parse validation
-  const { target } = input as any;
 
   if (typeof target !== 'string') {
     throw new ContractError('Must specify target to get balance for');
