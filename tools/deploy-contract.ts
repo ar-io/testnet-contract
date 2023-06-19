caa6f4c87008b683ed79498eb20a1b301a100574
dtfiedler <dylan@ar.io>
2023-06-19 16:39:07 -0600

    chore(deploy): add updated contract state values, cleanup buy record source code

--- tools/deploy-contract.ts
+++ tools/deploy-contract.ts
@@ -1,3 +1,4 @@
+import Arweave from 'arweave';
 import * as fs from 'fs';
 import path from 'path';
 import {
@@ -10,7 +11,6 @@ import { DeployPlugin } from 'warp-contracts-plugin-deploy';
 
 import { IOState } from '../src/types';
 import { keyfile } from './constants';
-import Arweave from 'arweave';
 
 (async () => {
   // ~~ Initialize `LoggerFactory` ~~
@@ -18,8 +18,8 @@ import Arweave from 'arweave';
   const arweave = new Arweave({
     host: 'ar-io.dev',
     port: 443,
-    protocol: 'https'
-  })
+    protocol: 'https',
+  });
 
   // ~~ Initialize SmartWeave ~~
   const warp = WarpFactory.forMainnet(
@@ -28,7 +28,7 @@ import Arweave from 'arweave';
       inMemory: true,
     },
     true,
-    arweave
+    arweave,
   ).use(new DeployPlugin());
 
   // Get the key file used for the distribution
@@ -41,17 +41,196 @@ import Arweave from 'arweave';
   );
 
   // load state of contract
-  const TEST_ARNS_CONTRACT_TX_ID = 'bLAgYxAdX2Ry-nt6aH2ixgvJXbpsEYm28NgJgyqfs-U';
-  const { cachedValue: { state: existingContractState }} = await warp.contract(TEST_ARNS_CONTRACT_TX_ID).readState();
-  
+  const TEST_ARNS_CONTRACT_TX_ID =
+    'bLAgYxAdX2Ry-nt6aH2ixgvJXbpsEYm28NgJgyqfs-U';
+  const {
+    cachedValue: { state: existingContractState },
+  } = await warp.contract(TEST_ARNS_CONTRACT_TX_ID).readState();
+  const { approvedANTSourceCodeTxs, evolve, ...relevantState } =
+    existingContractState as any;
   const forkedState = {
-    ...(existingContractState as IOState), 
+    ...(relevantState as IOState),
+    ticker: 'ARNS-TEST-V2',
     balances: {
       [walletAddress]: 10_000_000_000_000,
       'ZjmB2vEUlHlJ7-rgJkYP09N5IzLPhJyStVrK5u9dDEo': 10_000_000_000,
       '1H7WZIWhzwTH9FIcnuMqYkTsoyv1OTfGa_amvuYwrgo': 10_000_000_000,
-    }
-  }
+      '7waR8v4STuwPnTck1zFVkQqJh5K9q9Zik4Y5-5dV7nk': 10_000_000_000,
+      KsUYFIGvpX9MCbhHvsHbPAnxLIMYpzifqNFtFSuuIHA: 10_000_000_000,
+      'Kaajvkd2G--bS4qzKKECP1b2meEotzLwTPSoprSaQ_E': 10_000_000_000,
+      q6zIf3KQRMCW9fytR0YlKG4oqw6Cox4r_bk7mq6JZBM: 10_000_000_000,
+      QGWqtJdLLgm2ehFWiiPzMaoFLD50CnGuzZIPEdoDRGQ: 10_000_000_000,
+    },
+    reserved: {
+      www: {},
+      google: {
+        target: 'ZjmB2vEUlHlJ7-rgJkYP09N5IzLPhJyStVrK5u9dDEo',
+      },
+      microsoft: {
+        target: 'ZjmB2vEUlHlJ7-rgJkYP09N5IzLPhJyStVrK5u9dDEo',
+      },
+      apple: {
+        target: 'KsUYFIGvpX9MCbhHvsHbPAnxLIMYpzifqNFtFSuuIHA',
+      },
+      adobe: {
+        target: '7waR8v4STuwPnTck1zFVkQqJh5K9q9Zik4Y5-5dV7nk',
+      },
+      news: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      atticus: {
+        target: '7waR8v4STuwPnTck1zFVkQqJh5K9q9Zik4Y5-5dV7nk',
+      },
+      turbo: {
+        target: '1H7WZIWhzwTH9FIcnuMqYkTsoyv1OTfGa_amvuYwrgo',
+      },
+      july: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      august: {
+        endTimestamp: Math.floor(new Date('08/01/2023').getTime() / 1000),
+      },
+      september: {
+        endTimestamp: Math.floor(new Date('09/01/2023').getTime() / 1000),
+      },
+      phil: {
+        target: 'QGWqtJdLLgm2ehFWiiPzMaoFLD50CnGuzZIPEdoDRGQ',
+      },
+      mataras: {
+        target: 'QGWqtJdLLgm2ehFWiiPzMaoFLD50CnGuzZIPEdoDRGQ',
+      },
+      arielmelendez: {
+        endTimestamp: Math.floor(new Date('06/21/2023').getTime() / 1000),
+      },
+      one: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      two: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      four: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      five: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      six: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      seven: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      eight: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      nine: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      ten: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      admin: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      amazon: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      alexa: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      android: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      bestbuy: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      boston: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      blackrock: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      biogen: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      buzzfeed: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      eth: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      ethereum: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      coinbase: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      costco: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      disney: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      edison: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      facebook: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      files: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      help: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      meta: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      chatgpt: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      faq: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      netflix: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      nginx: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      nvidia: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      python: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      spotify: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      tinyurl: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      youtube: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      weather: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      verizon: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      zoo: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      openai: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+      llm: {
+        endTimestamp: Math.floor(new Date('07/01/2023').getTime() / 1000),
+      },
+    },
+  };
+  // TODO: do some AJV validation the the initial state meets our spec
   // ~~ Deploy contract ~~
   const contractTxId = await warp.deploy(
     {
@@ -71,8 +250,8 @@ import Arweave from 'arweave';
           maxInteractionEvaluationTimeSeconds: 60, // TODO: we may want to set this, doesn't look like anything by default
           allowBigInt: false, // big int cannot be serialized to JSON, but with Key/Value store, maybe we set this to true? TODO: determine if we want BigInt
           throwOnInternalWriteError: true,
-        }
-      }
+        },
+      },
     },
     true,
   ); // disable bundling
