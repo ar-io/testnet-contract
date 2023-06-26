b9f1dac66c8c8c05acb54f0e9eebda2b7d5143cc
dtfiedler <dylan@ar.io>
2023-06-26 13:15:55 -0600

    chore: evolve

--- src/actions/write/updateState.ts
+++ src/actions/write/updateState.ts
@@ -1,17 +1,16 @@
-import { ContractResult, IOState, PstAction } from "../../types.js";
-
+import { ContractResult, IOState, PstAction } from '../../types.js';
 
 // TODO: a temporary helper function while we validate state contract interactions
 export const updateState = (
-    state: IOState,
-    { input }: PstAction,
-  ): ContractResult => {
-    const { state: newState } = input;
+  state: IOState,
+  { input }: PstAction,
+): ContractResult => {
+  const { state: newState } = input;
 
-    return {
-        state: {
-            ...state, 
-            ...newState
-        }
-    }
-}
+  return {
+    state: {
+      ...state,
+      ...newState,
+    },
+  };
+};
