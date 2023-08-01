fc41cb481c9a363eec7398177e8d6b31964d412a
atticusofsparta <atticusofsparta@protonmail.com>
2023-08-01 09:10:31 -0600

    fix: update contractTxId in auction schema to include atomic flag

---
+++ tools/constants.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.testKeyfile = exports.keyfile = void 0;
+// The mainnet wallet that will be used in the creation of contracts, ants and record purchases.
+exports.keyfile = 'key.json';
+// The Redstone Testnet wallet that will be used in the creation of contracts, ants and record purchases.
+exports.testKeyfile = '';
