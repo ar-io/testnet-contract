2c0c96fb30e404ffec8a6c0030c38eb97ec5aed3
vilenarios <vilenarios@gmail.com>
2023-08-16 15:27:24 -0400

    Removed references for delegated staking

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
 import { AuctionSettings } from './types';
 
-export const MAX_DELEGATES = 1000; // the maximum amount of delegates that can be added to a single gateway
 export const MAX_YEARS = 5; // the maximum amount of years an arns name could be leased for
 export const MAX_NAME_LENGTH = 32; // the maximum length of an arns name
 export const MAX_NOTE_LENGTH = 256; // the maximum size of a note field
