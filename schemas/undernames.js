88e1102b6a07ea2f9ec2303a19fb1b1b61792720
atticusofsparta <atticusofsparta@protonmail.com>
2023-08-18 13:16:02 -0600

    fix: formatting

--- schemas/undernames.js
+++ schemas/undernames.js
@@ -1,25 +1,24 @@
 const increaseUndernamesSchema = {
-    $id: '#/definitions/increaseUndernames',
-    type: 'object',
-    properties: {
-      function: {
-        type: 'string',
-        const: 'increaseUndernames',
-      },
-      name: {
-        type: 'string',
-        pattern: '^([a-zA-Z0-9][a-zA-Z0-9-]{0,49}[a-zA-Z0-9]|[a-zA-Z0-9]{1})$',
-      },
-      qty: {
-        type: 'number',
-        minimum: 1,
-      },
+  $id: '#/definitions/increaseUndernames',
+  type: 'object',
+  properties: {
+    function: {
+      type: 'string',
+      const: 'increaseUndernames',
     },
-    required: ['name', 'qty'],
-    additionalProperties: false,
-  };
-  
-  module.exports = {
-    increaseUndernamesSchema,
-  };
-  
+    name: {
+      type: 'string',
+      pattern: '^([a-zA-Z0-9][a-zA-Z0-9-]{0,49}[a-zA-Z0-9]|[a-zA-Z0-9]{1})$',
+    },
+    qty: {
+      type: 'number',
+      minimum: 1,
+    },
+  },
+  required: ['name', 'qty'],
+  additionalProperties: false,
+};
+
+module.exports = {
+  increaseUndernamesSchema,
+};
