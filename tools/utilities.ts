c34acbbdf4d3bdde600cd98c41d79db14655c227
dtfiedler <dylan@ar.io>
2023-08-30 13:47:26 -0600

    chore: remove updateState function

--- tools/utilities.ts
+++ tools/utilities.ts
@@ -45,14 +45,3 @@ export async function retryFetch(reqURL: string): Promise<AxiosResponse<any>> {
   });
   return await axiosInstance.get(reqURL);
 }
-
-// Gets the latest block height
-export async function getCurrentBlockHeight() {
-  const { status, data: height } = await retryFetch(
-    `https://arweave.net/height`,
-  );
-  if (status !== 200) {
-    throw Error('Unable to fetch current height.');
-  }
-  return height;
-}
