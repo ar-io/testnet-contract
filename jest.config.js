e485bbbcd142f844f853a9bde49664b2860f097d
dtfiedler <dylan@ar.io>
2023-11-13 12:17:41 -0700

    chore(tests): move integration test configuration to command
    Makes unit tests faster.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   clearMocks: true,
-  globalSetup: './tests/setup.jest.ts',
-  globalTeardown: './tests/teardown.jest.ts',
   setupFilesAfterEnv: ['./tests/mocks.jest.ts'],
   moduleFileExtensions: ['ts', 'js', 'mjs'],
   collectCoverageFrom: [
