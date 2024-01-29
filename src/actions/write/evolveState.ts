9a50c95fcef0e75375cb3908858117875a9db56b
dtfiedler <dylan@ar.io>
2024-01-29 12:14:25 -0700

    chore(evolveState): update evolveState so these transformations can be applied on the state after evolved

--- src/actions/write/evolveState.ts
+++ src/actions/write/evolveState.ts
@@ -1,11 +1,14 @@
 import {
-  DEFAULT_GATEWAY_PERFORMANCE_STATS,
-  INITIAL_EPOCH_DISTRIBUTION_DATA,
-  INITIAL_PROTOCOL_BALANCE,
+  EPOCH_BLOCK_LENGTH,
   NON_CONTRACT_OWNER_MESSAGE,
 } from '../../constants';
-import { safeTransfer } from '../../transfer';
-import { ContractWriteResult, IOState, PstAction } from '../../types';
+import { getEpochDataForHeight } from '../../observers';
+import {
+  BlockHeight,
+  ContractWriteResult,
+  IOState,
+  PstAction,
+} from '../../types';
 
 // Updates this contract to new source code
 export const evolveState = async (
@@ -18,50 +21,26 @@ export const evolveState = async (
     throw new ContractError(NON_CONTRACT_OWNER_MESSAGE);
   }
 
-  // bump the protocol balance to the initial amount
-  if (
-    state.balances[owner] >= INITIAL_PROTOCOL_BALANCE &&
-    state.balances[SmartWeave.contract.id] < INITIAL_PROTOCOL_BALANCE
-  ) {
-    const protocolBalance = state.balances[SmartWeave.contract.id] || 0;
-    const differenceFromRequiredMinimum =
-      INITIAL_PROTOCOL_BALANCE - protocolBalance;
-    safeTransfer({
-      balances: state.balances,
-      fromAddress: owner,
-      toAddress: SmartWeave.contract.id,
-      qty: differenceFromRequiredMinimum,
-    });
-  }
+  const {
+    epochStartHeight,
+    epochEndHeight,
+    epochPeriod,
+    epochDistributionHeight,
+  } = getEpochDataForHeight({
+    currentBlockHeight: new BlockHeight(+SmartWeave.block.height),
+    epochZeroStartHeight: new BlockHeight(
+      state.distributions.epochZeroStartHeight,
+    ),
+    epochBlockLength: new BlockHeight(EPOCH_BLOCK_LENGTH),
+  });
 
-  // set the gateway stats
-  const updatedGateways = Object.entries(state.gateways).reduce(
-    (acc, [gatewayAddress, gateway]) => {
-      const stats = DEFAULT_GATEWAY_PERFORMANCE_STATS;
-      const updatedGatewayWithStats = {
-        stats,
-        ...gateway, // put this last so any existing stats persist
-      };
-      // set the gateway stats
-      return {
-        ...acc,
-        [gatewayAddress]: updatedGatewayWithStats,
-      };
-    },
-    {},
-  );
-  state.gateways = updatedGateways;
-
-  // set the distributions and observations
-  if (!state.distributions) {
-    // set up the distributions
-    state.distributions = {
-      ...INITIAL_EPOCH_DISTRIBUTION_DATA,
-    };
-
-    // reset the observations
-    state.observations = {};
-  }
+  state.distributions = {
+    epochZeroStartHeight: state.distributions.epochZeroStartHeight,
+    epochStartHeight: epochStartHeight.valueOf(),
+    epochEndHeight: epochEndHeight.valueOf(),
+    epochPeriod: epochPeriod.valueOf(),
+    nextDistributionHeight: epochDistributionHeight.valueOf(),
+  };
 
   return { state };
 };
